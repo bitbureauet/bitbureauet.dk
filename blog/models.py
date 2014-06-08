31d956b99751b613f160c32bbb353a4b9f97fe74
Víðir Valberg Guðmundsson <valberg@orn.li>
2014-06-08 15:56:48 +0200

    More template stuff.

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.utils.text import slugify
 
 
 class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
-
     updated_at = models.DateTimeField(auto_now_add=True, auto_now=True)
 
     title = models.CharField(max_length=255)
+
+    slug = models.SlugField()
 
     body = models.TextField()
 
@@ -19,3 +21,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post-detail', kwargs={'pk': self.pk})
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = patterns('',
    url(r'^$', views.PostList.as_view(), name='post-list'),
-   url(r'^(?P<pk>\d+)/$', views.PostDetail.as_view(), name='post-detail'),
-   url(r'^(?P<pk>\d+)/edit/$', views.PostUpdate.as_view(), name='post-update'),
    url(r'^new/$', views.PostCreate.as_view(), name='post-create'),
+   url(r'^(?P<slug>[\w-]+)/$', views.PostDetail.as_view(), name='post-detail'),
+   url(r'^(?P<slug>[\w-]+)/edit/$', views.PostUpdate.as_view(), name='post-update'),
 )
