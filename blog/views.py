ab47f82a2b62817a9499f0145b1dffa441cdd8f7
Víðir Valberg Guðmundsson <valberg@orn.li>
2015-01-10 21:43:32 +0100

    Little work on blog list and detail

--- blog/views.py
+++ blog/views.py
@@ -14,3 +14,8 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = models.Post
     template_name = 'blog/post_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_posts'] = models.Post.objects.all()[:5]
+        return context
